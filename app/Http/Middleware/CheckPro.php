f36777009c4c8e9c89ec9a9775d18c5baf01b186
yangxiaokai <2483701544@qq.com>
2020-06-28 19:21:49 +0800

    修改

--- app/Http/Middleware/CheckPro.php
+++ app/Http/Middleware/CheckPro.php
@@ -20,14 +20,14 @@ class CheckPro
         $request_url = $_SERVER['REQUEST_URI'];
 
         $url_hash = substr(md5($request_url),5,10);
-
-        $expire = 60;
-
+        $max = env('API_ACCESS_MAX');
+        $expire = env('API_ACCESS_TIMEOUT');
+        $time_last = env('API_ACCESS_TiME_LAST');
         $key = 'access_token_'.$url_hash;
 
         $token = Redis::get($key);
 //        echo $token;die;
-        if($token > 9){
+        if($token > $max){
             $response = [
                 'error' => 50010,
                 'msg' => "请求过于频繁，请在 {$expire} 秒后重试"
@@ -35,11 +35,13 @@ class CheckPro
 
             Redis::expire($key,$expire);
             echo json_encode($response,JSON_UNESCAPED_UNICODE);die;
+//            return $response()->json($response);
         }else{
                 $num = Redis::incr($key);
+                Redis::expire($key,$time_last);
 //                echo '当前访问次数为：'.$num;
-            return $next($request);
-        }
 
+        }
+        return $next($request);
     }
 }
